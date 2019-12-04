3392bda669922cba12fec52839b5792bde0f9aef
Noordeen Malango <noordinmalango@gmail.com>
2019-12-04 16:00:28 +0200

    reviews imdb polarity

---
+++ reviews-imdb-polarity.py
@@ -0,0 +1,76 @@
+#to solve the picklee issue
+import numpy as np
+# save np.load
+np_load_old = np.load
+
+# modify the default parameters of np.load
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+
+from keras.datasets import imdb
+
+(train_data, train_labels),(test_data, test_labels) = imdb.load_data(num_words=10000)
+# restore np.load for future normal usage
+np.load = np_load_old
+#print(train_data[0])
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences),dimension))
+    for i, sequence in enumerate(sequences):
+        results[i,sequence] = 1
+    return results
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+#print(x_train[0])
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+from keras import models
+from keras import layers
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+history = model.fit(partial_x_train,partial_y_train,epochs=20,batch_size=512,validation_data=(x_val, y_val))
+
+#lets see what happened during the training
+history_dict = history.history
+print(history_dict.keys())
+
+#plotting the training and validation loss
+import matplotlib.pyplot as plt 
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+
+epochs = range(1, len(history_dict['acc']) + 1)
+
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and Validation loss')
+
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+#Plotting the training and validation accuracy
+plt.clf()
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+plt.plot(epochs, acc_values, 'bo', label='Training acc')
+plt.plot(epochs, val_acc_values, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
